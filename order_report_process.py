b8ffb4fe0355ee651691d6d0308790027cc40b8e
Ubuntu <ubuntu@ip-172-31-1-197.ap-south-1.compute.internal>
2023-06-27 07:26:43 +0000

    updated postapi with email and wa

--- order_report_process.py
+++ order_report_process.py
@@ -4,9 +4,36 @@ import pandas as pd
 
 def get_order_details(df):
     if not os.path.exists(os.path.join(os.get_cwd(), 'order_tracker.csv')):
-        tracker_df = pd.DataFrame({'unique_id': [''], 'email_status': [''], 'whatsapp_status': []})
+        tracker_df = pd.DataFrame({'unique_id': [], 'name': [], 'phone_num': [], 'email_id': [], 'awb': [], 
+                                   'email_status': [], 'whatsapp_status': []})
+        new_ids = set(df['Order Id'])
     else:
-        unique_ids = list(tracker_df['unique_id'])
+        unique_ids = set(tracker_df['unique_id'])
+        new_ids = set(df['Order Id']).difference(unique_ids)
+    
+    to_be_pushed = []
+    trackerdf = []
+    for idx, row in df.iterrows():
+        dfdict = {}
+        if row['Order Id'] in new_ids:
+            phone_num = ''.join(''.join(str(row['Phone']).split(' '))).split('+')
+            phone_num = '91' + phone_num if len(phone_num)!=12 else phone_num
+            dfdict['unique_id'] = row['Order Id']
+            dfdict['name'] = str(['Customer Name'])
+            dfdict['email_id'] = str(row['Customer Email'])
+            dfdict['phone_num'] = phone_num
+            dfdict['awb'] = str(row['Courier Tracking Number'])
+            to_be_pushed.append(dfdict)
+            dfdict['email_status'] = ''
+            dfdict['whatsapp_status'] = ''
+            trackerdf.append(dfdict)
+    del df
+    to_be_pushed_df = pd.DataFrame(to_be_pushed)
+    del to_be_pushed
+    tracker_df = pd.DataFrame(trackerdf)
+    del trackerdf
+    tracker_df.to_csv(os.path.join(os.get_cwd(), 'order_tracker.csv'))
+    return to_be_pushed_df
 
 
         
--- orders_csv_postapi.py
+++ orders_csv_postapi.py
@@ -2,10 +2,13 @@ from flask import Flask, request
 from flask_restx import Api, Resource, fields
 from werkzeug.datastructures import FileStorage
 import pandas as pd
+import os
 from order_report_process import get_order_details
+from email_sender import send_dispatch_email
+from wati_apis import WATI_APIS
 
 
-
+wati = WATI_APIS()
 app = Flask(__name__)
 api = Api(app, version='1.0', title='CSV API', description='API for processing CSV files')
 
@@ -25,13 +28,50 @@ class CSVProcessing(Resource):
         args = upload_parser.parse_args()
         csv_file = args['file']
         df = pd.read_csv(csv_file)
-        # Perform operations on the DataFrame here
-        #FOR SIMS
-        #list_of_entries = 
-        # Example: Calculate the sum of values in the 'column_name' column
-        print(df.columns)
+        to_be_pushed = get_order_details(df)
+        trackerdf = pd.read_csv(os.path.join(os.get_cwd(), 'order_tracker.csv'))
 
-        return {'sum': 'hi da'}
+        statuses = []
+
+        for idx, row in to_be_pushed.iterrows():
+            try:
+                id = row['unique_id']
+                email = row['email_id']
+                phone_num = row['phone_num']
+                name = row['name']
+                awb = row['awb']
+                ## send template message
+                try:
+                    custom_params=[{'name': 'awb', 'value': awb}]
+                    status = wati.send_template_message(contact_name=name, template_name='order_dispatched_with_awb', 
+                                            custom_params=custom_params)
+                    if not status:
+                        trackerdf[trackerdf['unique_id'] == id]['whatsapp_status'] = 'Failure'
+                        wa_status = 'Failure'
+                    else:
+                        trackerdf[trackerdf['unique_id'] == id]['whatsapp_status'] = 'Success'
+                        wa_status = 'Success'
+                except:
+                    trackerdf[trackerdf['unique_id'] == id]['whatsapp_status'] = 'Failure_exception'
+                    wa_status = 'Failure_exception'
+                
+                ## send email
+                try:
+                    status = send_dispatch_email(name= name, to_address= email, awb_number=awb)
+                    trackerdf[trackerdf['unique_id'] == id]['email_status'] = status
+                    email_status = status
+                except:
+                    trackerdf[trackerdf['unique_id'] == id]['email_status'] = 'Failure_exception'
+                    email_status = 'Failure_exception'
+                statuses.append({'id': id, 'email_status': email_status, 'wa_status': wa_status})
+            except:
+                trackerdf.to_csv(os.path.join(os.get_cwd(), 'order_tracker.csv'))
+            
+
+        trackerdf.to_csv(os.path.join(os.get_cwd(), 'order_tracker.csv'))
+
+
+        return statuses
 
 
 if __name__ == '__main__':
@@ -44,4 +84,4 @@ if __name__ == '__main__':
         return response
 
 
-    app.run(debug=True, port = 5000)
+    app.run(debug=True, port = 5003)
