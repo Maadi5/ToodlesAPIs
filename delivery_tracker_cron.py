e0b669ec5bf48fd0a69236155fdbfd6e71b537f2
Maadi5 <ma.adithya55@gmail.com>
2023-10-13 12:55:47 +0530

    remove break statement

--- delivery_tracker_cron.py
+++ delivery_tracker_cron.py
@@ -459,7 +459,7 @@ print(times_to_run)
 ## Schedule the job to run every day at 3pm (test)
 for time_str in times_to_run:
     schedule.every().day.at(time_str).do(bluedart_tracking_checker)
-    break
+    # break
 #
 print('running cron...')
 while True:
